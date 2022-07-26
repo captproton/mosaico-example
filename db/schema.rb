62c40e4e51befbc5c222ae271488f69240a466ea
captproton <carl@wdwhub.net>
2022-07-26 16:56:05 -0700

    ADDED: installed activestorage

--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,58 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220726025450) do
+ActiveRecord::Schema.define(version: 2022_07_26_235039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "mosaico_images", force: :cascade do |t|
-    t.string   "file",       null: false
-    t.integer  "width",      null: false
-    t.integer  "height",     null: false
-    t.integer  "filesize",   null: false
-    t.string   "mime_type",  null: false
-    t.string   "type",       null: false
-    t.integer  "parent_id"
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "mosaico_images", id: :serial, force: :cascade do |t|
+    t.string "file", null: false
+    t.integer "width", null: false
+    t.integer "height", null: false
+    t.integer "filesize", null: false
+    t.string "mime_type", null: false
+    t.string "type", null: false
+    t.integer "parent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "mosaico_projects", force: :cascade do |t|
-    t.text     "html"
-    t.text     "content"
-    t.text     "metadata"
-    t.string   "template_name"
+  create_table "mosaico_projects", id: :serial, force: :cascade do |t|
+    t.text "html"
+    t.text "content"
+    t.text "metadata"
+    t.string "template_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "projects", force: :cascade do |t|
-    t.string   "name"
+  create_table "projects", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
