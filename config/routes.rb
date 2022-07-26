42f1aa036880287eee56138290e6f8171b40c24f
captproton <carl@wdwhub.net>
2022-07-26 11:53:40 -0700

    CHANGE: upgrade rails from  5.0.7.2 to 5.1.7

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  resources :projects
-  root to: 'home#index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  mount Mosaico::Engine => '/mosaico'
 end
