9d61a3e0b3d5d93a9538c2d030225c486d1aa552
captproton <carl@wdwhub.net>
2022-07-26 18:07:33 -0700

    ADDED: Post scaffold to test proper installation of activestorage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_26_235039) do
+ActiveRecord::Schema.define(version: 2022_07_26_235802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,13 @@ ActiveRecord::Schema.define(version: 2022_07_26_235039) do
     t.string "template_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "projects", id: :serial, force: :cascade do |t|
